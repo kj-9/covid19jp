fe0c3498693f66bdfff8395eebe9ecf4b4f3a234
kj-9 <46800811+kj-9@users.noreply.github.com>
2020-12-29 12:28:25 +0900

    add pref_daily

---
+++ data-raw/pref_daily.R
@@ -0,0 +1,27 @@
+library(readr)
+library(dplyr)
+devtools::load_all()
+
+
+url <- "https://github.com/kaz-ogiwara/covid19/raw/master/data/prefectures.csv"
+pref_daily <- read_csv(url, na = "-") %>%
+  left_join(pref, by = c("prefectureNameJ" = "prefJP")) %>%
+  transmute(
+    prefCode,
+    prefJP = prefectureNameJ,
+    prefEN,
+    date = as.Date(paste(year, month, date, sep = "-")),
+    tests = peopleTested,
+    newCases = testedPositive,
+    activeCases = hospitalized,
+    severeCases = serious,
+    recovered = discharged,
+    deaths = deaths,
+    rt = effectiveReproductionNumber
+  ) %>%
+  arrange(prefCode, date)
+
+
+write_files(pref_daily, "data-raw/dist")
+
+usethis::use_data(pref_daily, compress = "gzip", overwrite = TRUE)
