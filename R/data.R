1de255c56ee020b4a7684d51507a427eca224617
kj-9 <46800811+kj-9@users.noreply.github.com>
2020-12-29 17:40:02 +0900

    add roxygen comments for datasets

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Japanese daily data of covid-19 situation
+#'
+#' @format A data frame. See detail: \url{https://github.com/kj-9/covid19jp/}
+"jp_daily"
+
+
+#' Master data for Japanese prefectures
+#'
+#' @format A data frame. See detail: \url{https://github.com/kj-9/covid19jp/}
+"pref"
+
+#' Japanese prefecture-level daily data of covid-19 situation
+#'
+#' @format A data frame. See detail: \url{https://github.com/kj-9/covid19jp/}
+"pref_daily"
+
+#' Japanese prefecture-level weekly data of covid-19 situation
+#'
+#' @format A data frame. See detail: \url{https://github.com/kj-9/covid19jp/}
+"pref_weekly"
+
