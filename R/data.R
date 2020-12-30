4afe4c4a627f183dde9c1a371de60f3601828dd4
kj-9 <46800811+kj-9@users.noreply.github.com>
2020-12-30 13:44:49 +0900

    update-data: update comments

--- R/data.R
+++ R/data.R
@@ -1,21 +1,25 @@
 #' Japanese daily data of covid-19 situation
 #'
-#' @format A data frame. See detail: \url{https://github.com/kj-9/covid19jp/}
+#' @format A data frame.
+#' @seealso More details are available: \url{https://github.com/kj-9/covid19jp/}
 "jp_daily"
 
 
 #' Master data for Japanese prefectures
 #'
 #' @format A data frame. See detail: \url{https://github.com/kj-9/covid19jp/}
+#' @seealso More details are available: \url{https://github.com/kj-9/covid19jp/}
 "pref"
 
 #' Japanese prefecture-level daily data of covid-19 situation
 #'
 #' @format A data frame. See detail: \url{https://github.com/kj-9/covid19jp/}
+#' @seealso More details are available: \url{https://github.com/kj-9/covid19jp/}
 "pref_daily"
 
 #' Japanese prefecture-level weekly data of covid-19 situation
 #'
 #' @format A data frame. See detail: \url{https://github.com/kj-9/covid19jp/}
+#' @seealso More details are available: \url{https://github.com/kj-9/covid19jp/}
 "pref_weekly"
 
