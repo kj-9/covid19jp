50a3b65509f06c18e1d259b6b5330f339cea28af
kj-9 <46800811+kj-9@users.noreply.github.com>
2021-01-11 23:40:31 +0900

    update roxygen comment; need to add new line to correctly desplay

--- R/data.R
+++ R/data.R
@@ -1,25 +1,28 @@
 #' covid19jp: Japanese Covid-19 Datasets
 #'
-#' The covid19jp package provides ready to use Japanese covid-19 datasets. More
-#' details are available
-#' \href{https://github.com/kj-9/covid19jp#dataset-detail}{here}.
+#' The covid19jp package provides ready to use Japanese covid-19 datasets.
+#' More details are available \href{https://github.com/kj-9/covid19jp#dataset-detail}{here}.
 #'
 #' @docType package
 #' @name covid19jp
 NULL
 
-#' Japanese daily data of covid-19 situation. More details are available
-#' \href{https://github.com/kj-9/covid19jp#japan_daily}{here}.
+#' Japanese daily data of covid-19 situation.
+#'
+#' More details are available \href{https://github.com/kj-9/covid19jp#japan_daily}{here}.
 "jp_daily"
 
-#' Master data for Japanese prefectures. More details are available
-#' \href{https://github.com/kj-9/covid19jp#pref}{here}.
+#' Master data for Japanese prefectures.
+#'
+#' More details are available \href{https://github.com/kj-9/covid19jp#pref}{here}.
 "pref"
 
-#' Japanese prefecture-level daily data of covid-19 situation. More details are
-#' available \href{https://github.com/kj-9/covid19jp#pref_daily}{here}.
+#' Japanese prefecture-level daily data of covid-19 situation.
+#'
+#' More details are available \href{https://github.com/kj-9/covid19jp#pref_daily}{here}.
 "pref_daily"
 
-#' Japanese prefecture-level weekly data of covid-19 situation. More details are
-#' available \href{https://github.com/kj-9/covid19jp#pref_weekly}{here}.
+#' Japanese prefecture-level weekly data of covid-19 situation.
+#'
+#' More details are available \href{https://github.com/kj-9/covid19jp#pref_weekly}{here}.
 "pref_weekly"
