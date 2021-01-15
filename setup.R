840b1211e9b1883e12c2d34d107f3953fe6d9407
GitHub Action <action@github.com>
2021-01-16 00:42:37 +0900

    separate system setup and R package setup: need to refresh R session aftter system setup

--- setup.R
+++ setup.R
@@ -1,19 +1,3 @@
-# example: Rscript setup.R ubuntu 20.04
-# then, args <-  c("ubuntu", "20.04")
-
-install.packages("remotes")
-
-args <- commandArgs(trailingOnly = TRUE)
-cmds <- remotes::system_requirements(args[1], args[2])
-
-for (cmd in cmds) {
-  system(paste("sudo", cmd))
-}
-
-# install jre
-system("sudo apt-get install -y default-jre")
-
-# install pre-build dependencies
 install.packages(
   c(
     "usethis",
@@ -31,6 +15,4 @@ install.packages(
     "modules"
   )
 )
-
-# install package dependencies
 remotes::install_deps(dependencies = TRUE)
