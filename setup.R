dfd22b988ed3abf5b4ee09e1c5aab17443ac82d7
GitHub Action <action@github.com>
2021-01-15 23:40:24 +0900

    add missing dependency, update cache logic

--- setup.R
+++ setup.R
@@ -1,8 +1,9 @@
 # example: Rscript setup.R ubuntu 20.04
 # then, args <-  c("ubuntu", "20.04")
 
+install.packages("remotes")
+
 args <- commandArgs(trailingOnly = TRUE)
-print(args)
 cmds <- remotes::system_requirements(args[1], args[2])
 
 for (cmd in cmds) {
@@ -12,15 +13,14 @@ for (cmd in cmds) {
 # install jre
 system("sudo apt-get install -y default-jre")
 
-# install package dependencies
-remotes::install_deps(dependencies = TRUE)
-
 # install pre-build dependencies
 install.packages(
   c(
+    "remotes",
     "purrr",
     "dplyr",
     "readr",
+    "tidyr",
     "rvest",
     "stringr",
     "stringi",
@@ -31,3 +31,6 @@ install.packages(
     "modules"
   )
 )
+
+# install package dependencies
+remotes::install_deps(dependencies = TRUE)
