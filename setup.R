c7fbae79149a1e9ff5061a83dbbb775f0024243f
GitHub Action <action@github.com>
2021-01-16 13:49:32 +0900

    install packages only needed

--- setup.R
+++ setup.R
@@ -1,18 +1,18 @@
-install.packages(
-  c(
-    "usethis",
-    "purrr",
-    "dplyr",
-    "readr",
-    "tidyr",
-    "rvest",
-    "stringr",
-    "stringi",
-    "xml2",
-    "rJava",
-    "tabulizer",
-    "log4r",
-    "modules"
-  )
+remotes::update_packages(c(
+  "usethis",
+  "purrr",
+  "dplyr",
+  "readr",
+  "tidyr",
+  "rvest",
+  "stringr",
+  "stringi",
+  "xml2",
+  "rJava",
+  "tabulizer",
+  "log4r",
+  "modules"
+),
+upgrade = "always"
 )
 remotes::install_deps(dependencies = TRUE)
