7f807ce1999d43a3a1af0dea908c0e7470335fa2
kj-9 <46800811+kj-9@users.noreply.github.com>
2021-07-14 15:05:02 +0900

    format code

--- setup.R
+++ setup.R
@@ -1,19 +1,20 @@
-remotes::update_packages(c(
-  "usethis",
-  "purrr",
-  "dplyr",
-  "readr",
-  "tidyr",
-  "rvest",
-  "stringr",
-  "stringi",
-  "xml2",
-  "rJava",
-  "tabulizer",
-  "log4r",
-  "box",
-  "devtools"
-),
-upgrade = "always"
+remotes::update_packages(
+  c(
+    "usethis",
+    "purrr",
+    "dplyr",
+    "readr",
+    "tidyr",
+    "rvest",
+    "stringr",
+    "stringi",
+    "xml2",
+    "rJava",
+    "tabulizer",
+    "log4r",
+    "box",
+    "devtools"
+  ),
+  upgrade = "always"
 )
 remotes::install_deps(dependencies = TRUE)
